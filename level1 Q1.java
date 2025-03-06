39af66f26248782c2aa23bf2044480cfc44cf66b
Arundhati Singh <arundhatisingh10@gmail.com>
2025-03-06 14:11:52 +0530

    Add files via upload

---
+++ level1 Q1.java
@@ -0,0 +1,8 @@
+public class harry{
+	public static void main(String[] args) {
+		int birth_year=2000;
+		int current_year=2024;
+		int age=current_year - birth_year;
+		System.out.println("Harry's age in "+current_year+" is: "+age);
+	}
+}
