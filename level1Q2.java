85d298ebbbc13e9193bb514f99aab2a0ef5d243f
Arundhati Singh <arundhatisingh10@gmail.com>
2025-03-06 14:12:56 +0530

    Add files via upload

---
+++ level1Q2.java
@@ -0,0 +1,10 @@
+class level1Q2{
+	public static void main(String[]args) {
+		int maths_marks=94;
+		int physics_marks=95;
+		int chemistry_marks=96;
+		int total_marks= maths_marks+physics_marks+chemistry_marks;
+		double average_marks=total_marks/3.0;
+		System.out.println("Sam's average mark in PCM is" +average_marks);
+	}
+}
